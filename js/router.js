b47a0d4ac2c3f223e5a8f0cb133f574aa0e52db5
Tawni Martin <tawni@ideate-interactive.com>
2015-04-11 18:51:55 -0400

    updates to home page

--- js/router.js
+++ js/router.js
@@ -162,8 +162,11 @@ var Router = Backbone.Router.extend ({
   },
   home: function() {
     //
-  $("html").css({ "background"  : "none" });
   $(".home").html( this.homeView.render().el );
   $( ".content" ).hide();
+  $('#intro').parallax("50%", 0.1);
+  $('#second').parallax("50%", 0.1);
+  $('.bg').parallax("50%", 0.4);
+  $('#third').parallax("50%", 0.3);
   }
 });
