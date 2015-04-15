978d0bb1889d80c37bf8060bc61430cc874e3dce
Tawni Martin <tawni@ideate-interactive.com>
2015-04-15 14:03:32 -0400

    bug fixing

--- js/main.js
+++ js/main.js
@@ -3,12 +3,3 @@ $(function() {
 	window.router = new Router();
 	Backbone.history.start();
 });
-
-
-// var svg = d3.select("svg");
-
-// var eq = new Eq(svg);
-// eq.start();
-
-// React.render(React.createElement(ReactEQ, {play=true}),
-	// document.body);
